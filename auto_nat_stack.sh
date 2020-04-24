781ec43466eb79920d75800a60bd429ae25fa88a
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-04-24 15:45:06 +0800

    Update auto_nat_stack.sh

--- auto_nat_stack.sh
+++ auto_nat_stack.sh
@@ -5,39 +5,57 @@ source /home/heat-admin/overcloudrc
 # Current folder
 local_path=`pwd`
 
+OSP_PID=`echo $$`
 
 function getStack(){
-    STACK=`openstack stack list | egrep -v "^#|^$|^\+" | grep "CREATE_COMPLETE" | sed s/[[:space:]]//g`
-    if [[ "$STACK" != "" ]]; then
-        STACK
-    fi
-}
-
-function getPID(){
-    OSP_PID=`echo $$`
-    echo $STACK > ${local_path}/tmp_${OSP_PID}.log
+    openstack stack list | egrep -v "^#|^$|^\+" | grep "CREATE_COMPLETE" | sed s/[[:space:]]//g > ${local_path}/tmp_${OSP_PID}.log
 }
 
 function getLimit(){
-    STACK_LIMIT=`cat ${local_path}/tmp_${pid}.log | wc -l`
+    STACK_LIMIT=`cat ${local_path}/tmp_${OSP_PID}.log | wc -l`
+}
+
+# Get public IP
+function getIP(){
+    # Floating_IP
+    NUM=$(( $COUNT + 1 ))
+    STACK_ID[$COUNT]=`sed -n "${NUM}p" ${local_path}/tmp_${OSP_PID}.log | awk -F "|" '{print $2}'`
+    #echo "STACK ID : "${STACK_ID[$COUNT]}
+    FLOATING_IP[$COUNT]=`openstack stack show ${STACK_ID[$COUNT]} | grep  -A1 "output_key: server1_public_ip" | sed s/[[:space:]]//g | awk -F "|" '{print $3}' | grep "output_value" | awk -F ":" '{print $2}'`
+    #echo "FLOATING_IP : "${FLOATING_IP[$COUNT]}
+
+    PUBLIC_IP=`echo ${FLOATING_IP[$COUNT]} | egrep -o '[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[101-112]{1,3}' | head -n 1`
 }
 
 function getParameter(){
-    # Floating_IP
-    array_stack_id[$count]=`sed -n "${num}p" ${local_path}/tmp_${pid}.log | awk -F "|" '{print $2}'`
-    ext_ip[$count]=`openstack stack show ${array_stack_id[$count]} | grep  -A1 "output_key: server1_public_ip" | sed s/[[:space:]]//g | awk -F "|" '{print $3}' | grep "output_value" | awk -F ":" '{print $2}'`
+    # Catch Floating_IP ID
+    EXTERNAL_IP_ID[$COUNT]=`openstack floating ip list | grep ${FLOATING_IP[$COUNT]}" " | awk -F "|" '{print $2}' | sed s/[[:space:]]//g`
 
-
-
+    # Catch Floating IP Status
+    EXTERNAL_IP_STATUS[$COUNT]=`openstack floating ip show ${EXTERNAL_IP_ID[$COUNT]} | grep "status" | awk -F "|" '{print $3}' | sed s/[[:space:]]//g`
 }
 
-count=0
-num=1
-while [ $count -lt $STACK_LIMIT ]; do
+getStack
+getLimit
 
+COUNT=0
+while [ $COUNT -lt $STACK_LIMIT ]; do
+    getIP
+    if [[ "${PUBLIC_IP}" != "" ]]; then
+        echo "PUBLIC IP: "${PUBLIC_IP}
+        getParameter
+        if [[ "${EXTERNAL_IP_STATUS[$COUNT]}" == "ACTIVE" ]]; then
+                ### echo "Ext_IP:${EXTERNAL_IP_STATUS[$COUNT]}" >> ${local_path}/stack_${OSP_PID}.log
+                ### echo "Ext_IP_Status:${ext_ip_status[$count]}" >> ${local_path}/stack_${OSP_PID}.log
+                echo "${PUBLIC_IP[$COUNT]}" >> ${local_path}/stack_${OSP_PID}.log
+        fi
+    fi
 
-
-    (( count++ ))
-    (( num++ ))
+    (( COUNT++ ))
 done
 
+if [[ -f ${local_path}/stack_${OSP_PID}.log ]]; then
+    cp ${local_path}/stack_${OSP_PID}.log ${local_path}/stack_.log
+    sudo rm ${local_path}/stack_${OSP_PID}.log
+    sudo rm ${local_path}/tmp_${OSP_PID}.log
+fi
