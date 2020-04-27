b12c41180e47fab3c4559d7a565686c7bc7ce442
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-04-27 10:34:56 +0800

    Update auto_nat_stack.sh

--- auto_nat_stack.sh
+++ auto_nat_stack.sh
@@ -22,7 +22,7 @@ function getIP(){
     STACK_ID[$COUNT]=`sed -n "${NUM}p" ${local_path}/tmp_${OSP_PID}.log | awk -F "|" '{print $2}'`
 
     FLOATING_IP[$COUNT]=`openstack stack show ${STACK_ID[$COUNT]} | grep  -A1 "output_key: server1_public_ip" | sed s/[[:space:]]//g | awk -F "|" '{print $3}' | grep "output_value" | awk -F ":" '{print $2}'`
-    
+
     PUBLIC_IP=`echo ${FLOATING_IP[$COUNT]} | egrep '10\.[0-9]{1,3}\.[0-9]{1,3}\.10[1-9]|10\.[0-9]{1,3}\.[0-9]{1,3}\.11[1-2]' | egrep -v "^10\.241\.155\.[0-9]{1,3}" | head -n 1`
 }
 
@@ -37,6 +37,7 @@ function getParameter(){
 function getSchoolVlan(){
     PROJECT_ID=`openstack stack show ${STACK_ID[$COUNT]} | grep "OS::project_id: " | sed s/[[:space:]]//g | awk -F "|" '{print $3}' | awk -F "OS::project_id:" '{print $2}'`
     VLAN=`openstack project show ${PROJECT_ID} | grep "name" | sed s/[[:space:]]//g | awk -F "|" '{print $3}' | awk -F "_" '{print $1}'`
+    echo "VLAN: \""${VLAN}"\""
 }
 
 getStack
@@ -44,15 +45,15 @@ getLimit
 
 COUNT=0
 while [ $COUNT -lt $STACK_LIMIT ]; do
-    echo "#####################"
+    #echo "#####################"
     getIP
     if [[ "${PUBLIC_IP}" != "" ]]; then
         echo "PUBLIC IP: "${PUBLIC_IP}
-        #echo "VLAN: \""${VLAN}"\""
         getParameter
         if [[ "${EXTERNAL_IP_STATUS[$COUNT]}" == "ACTIVE" ]]; then
             getSchoolVlan
-            echo "${PUBLIC_IP}" >> ${local_path}/stack_${OSP_PID}.log
+            echo "VLAN: "${VLAN} >> ${local_path}/stack_${OSP_PID}.log
+            echo "IP: "${PUBLIC_IP} >> ${local_path}/stack_${OSP_PID}.log
         fi
     fi
 
