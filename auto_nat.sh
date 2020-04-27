880057cd0fe307554ca04fcdaf16db26029ac454
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-04-27 15:25:20 +0800

    Update auto_nat.sh

--- auto_nat.sh
+++ auto_nat.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+ANSIBLE_PASS="$1"
+ANSIBLE_HOST="$2"
+ANSIBLE_ROOT_PATH="$3"
+STACK_LOG="$4"
+F5_PATH="$5"
+
+getStack(){
+    sshpass -p ${ANSIBLE_PASS} scp root@${ANSIBLE_HOST}:${ANSIBLE_ROOT_PATH}/${STACK_LOG} $F5_PATH/
+}
+
+getStack
 
