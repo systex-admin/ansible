83301dba282baaa445b1d1c22c26b6f830c75ac3
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-04-27 18:41:11 +0800

    Update auto_nat.sh

--- auto_nat.sh
+++ auto_nat.sh
@@ -2,6 +2,8 @@
 
 # Current folder
 DIR=`pwd`
+NAT_PYTHON=$1
+NAT_PYTHON_DIR="${pwd}/${NAT_PYTHON}"
 
 # STACK LOG
 #STACK_LOG="$1"
@@ -18,10 +20,10 @@ getPrivateIP(){
     RRIVATE_IP=`cat auto_stack.log | grep "IP" | awk -F ": " '{print $2}' | head -n $NUM | tail -n 1`
 }
 
-
 COUNT=0
 #while [ $COUNT -lt $LIMIT ]; do
-while true ; do
+while true
+do
     if [ $COUNT -ge $LIMIT ]; then
         break
     fi
@@ -29,15 +31,25 @@ while true ; do
     if [ $COUNT -lt $LIMIT ]; then
         getVLAN
         getPrivateIP
+
         N=$(( $COUNT + 1 ))
-        echo "[${N}]"
-        #echo "P VLAN: "$VLAN
-        #echo "P RRIVATE IP: "$RRIVATE_IP
-        tmsh list ltm nat NAT_${RRIVATE_IP}
+        echo "${N}."
+        #echo "VLAN: "$VLAN
+        #echo "RRIVATE IP: "$RRIVATE_IP
+
+        NAT_LIST_MSG=`tmsh list ltm nat nat_${RRIVATE_IP}`
+                NAT_HAVE_MSG="inherited-traffic-group true"
+                NAT_RESULT=$(echo $NAT_LIST_MSG | grep "${NAT_HAVE_MSG}")
+                if [[ "${NAT_RESULT}" == "" ]] ; then
+                        if [[ -f ${NAT_PYTHON_DIR} ]]; then
+                                python ${NAT_PYTHON_DIR} ${VLAN} ${RRIVATE_IP} add nat_list.json
+                        fi
+                else
+                        echo "${RRIVATE_IP} EXIST NAT."
+                fi
+
         echo
     fi
 
     (( COUNT++ ))
 done
-
-
