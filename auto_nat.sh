56d12887e27969e081c06252aa810852d42e231e
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-04-27 16:40:58 +0800

    Update auto_nat.sh

--- auto_nat.sh
+++ auto_nat.sh
@@ -7,7 +7,7 @@ STACK_LOG="$3"
 F5_PATH="$4"
 
 getStack(){
-    scp -o StrictHostKeyChecking=no root@${ANSIBLE_HOST}:${ANSIBLE_ROOT_PATH}/${STACK_LOG} $F5_PATH/
+    scp -o StrictHostKeyChecking=no root@${ANSIBLE_HOST}:${ANSIBLE_ROOT_PATH}/${STACK_LOG} ${F5_PATH}/
 }
 
 getStack
