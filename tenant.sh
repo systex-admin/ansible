c4ea4814ee38c16e68930056951949c046ac808a
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-05-14 23:29:02 +0800

    Update tenant.sh

--- tenant.sh
+++ tenant.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-TENANT_LOG=$1
-PROJECT_VLAN=$2
-PROJECT_DESCRIPTION=$3
-PROJECT_EXT_SEGMENT_NUM=$4
-PROJECT_EXT_IP_24BIT=$5
-PROJECT_MANAGE_IP_24BIT=$6
 
-if [ -f ${TENANT_LOG} ]; then
-    sudo rm -rf ${TENANT_LOG}
-fi
+############################################
+###
+###     TEST
+###
+############################################
+#TENANT_LOG=$1
+#PROJECT_VLAN=$2
+#PROJECT_DESCRIPTION=$3
+#PROJECT_EXT_SEGMENT_NUM=$4
+#PROJECT_EXT_IP_24BIT=$5
+#PROJECT_MANAGE_IP_24BIT=$6
 
-echo "${PROJECT_VLAN}" >> ${TENANT_LOG}
-echo "${PROJECT_DESCRIPTION}"  >> ${TENANT_LOG}
-echo "${PROJECT_EXT_SEGMENT_NUM}"  >> ${TENANT_LOG}
-echo "${PROJECT_EXT_IP_24BIT}"  >> ${TENANT_LOG}
-echo "${PROJECT_MANAGE_IP_24BIT}"  >> ${TENANT_LOG}
+#if [ -f ${TENANT_LOG} ]; then
+#    sudo rm -rf ${TENANT_LOG}
+#fi
+
+#echo "${PROJECT_VLAN}" >> ${TENANT_LOG}
+#echo "${PROJECT_DESCRIPTION}"  >> ${TENANT_LOG}
+#echo "${PROJECT_EXT_SEGMENT_NUM}"  >> ${TENANT_LOG}
+#echo "${PROJECT_EXT_IP_24BIT}"  >> ${TENANT_LOG}
+#echo "${PROJECT_MANAGE_IP_24BIT}"  >> ${TENANT_LOG}
